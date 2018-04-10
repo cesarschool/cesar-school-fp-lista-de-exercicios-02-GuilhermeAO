d1d6ea2674f170f0bb8e0b760ad097da0e91c574
GuilhermeAO <38106184+GuilhermeAO@users.noreply.github.com>
2018-04-10 18:03:24 -0300

    questão 1

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -10,6 +10,32 @@
 ##
 def main():
     print("questao 1")
+    n1 = int(input("Digite o primeiro numero inteiro: "))
+n2 = int(input("Digite o segundo numero inteiro: "))
+n3 = int(input("Digite o terceiro numero inteiro: "))
+n4 = int (input("Digite o quarto numero inteiro: "))
+n5 = int(input("Digite o quinto numero inteiro: "))
+if n1 >= n2 and n1 >= n3 and n1 >= n4 and  n1 >= n5:
+    print("{} é o maior numero.".format(n1))
+elif n2 >= n1 and n2 >= n3 and n2 >= n4 and n2 >= n5:
+    print("{} é o maior numero.".format(n2))
+elif n3 >= n1 and n3 >= n2 and n3 >= n4 and n3 >= n5:
+    print("{} é o maior numero.".format(n3))
+elif n4 >= n1 and n4 >= n2 and n4 >= n3 and n4 >= n5:
+    print("{} é o maior numero.".format(n4))
+elif n5 >= n1 and n5 >= n2 and n5 >= n3 and n5 >= n4:
+    print("{} é o maior numero.".format(n5))
+
+if n1 <= n2 and n1 <= n3 and n1 <= n4 and n1 <= n5:
+    print("{} é o menor numero.".format(n1))
+elif n2 <= n1 and n2 <= n3 and n2 <= n4 and n2 <= n5:
+    print("{} é o menor numero.".format(n2))
+elif n3 <= n1 and n3 <= n2 and n3 <= n4 and n3 <= n5:
+    print("{} é o menor numero.".format(n3))
+elif n4 <= n1 and n4 <= n2 and n4 <= n3 and n4 <= n5:
+    print("{} é o menor numero.".format(n4))
+elif n5 <= n1 and n5 <= n2 and n5 <= n3 and n5 <= n4:
+    print("{} é o menor numero.".format(n5))
     
 
 

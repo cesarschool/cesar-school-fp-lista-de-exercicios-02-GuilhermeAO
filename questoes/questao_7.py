77db71cffd22296b5185ef065b72e34976cadce0
GuilhermeAO <38106184+GuilhermeAO@users.noreply.github.com>
2018-04-10 23:36:08 -0300

    questao_7.py

--- questoes/questao_7.py
+++ questoes/questao_7.py
@@ -21,7 +21,11 @@
 def main():
     print("questao 7")
 
-
+ano = int(input("Escreva o ano: "))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 ==0:
+    print("O ano informado ({}) é bissexto.".format(ano))
+else:
+    print("O ano informado ({}) não é bissexto.".format(ano))
     
 if __name__ == '__main__':
     main()

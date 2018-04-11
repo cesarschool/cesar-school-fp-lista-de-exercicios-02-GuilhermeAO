9d9eea59b1ed0d2e7ae88b5324a9deb70e77e5dc
GuilhermeAO <38106184+GuilhermeAO@users.noreply.github.com>
2018-04-10 23:28:04 -0300

    questao_6.py

--- questoes/questao_6.py
+++ questoes/questao_6.py
@@ -14,6 +14,31 @@
 def main():
     print("questao 6")
 
+    cadeia_dna = str(input("Digite a cadeia de DNA: "))
+cont_A = cadeia_dna.count("A")
+cont_C = cadeia_dna.count("C")
+cont_G = cadeia_dna.count("G")
+cont_T = cadeia_dna.count("T")
+soma_cont = cont_A + cont_T + cont_C + cont_G
+todos_carac = len(cadeia_dna)
+porc_A = (100 * cont_A) / todos_carac
+porc_C = (100 * cont_C) / todos_carac
+porc_G = (100 * cont_G) / todos_carac
+porc_T = (100 * cont_T) / todos_carac
+quant_inv = todos_carac - soma_cont
+porc_inv = (100 * quant_inv) / todos_carac
+if quant_inv == 0:
+    print("""A porcentagem de nucleotídeos A é: {}%
+        de nucleotídeos C é: {}%
+        de nucleotídeos G é: {}%
+        de nucleotídeos T é: {}%
+        Não tem nucleotídeos invalidos na cadeia de DNA informada.""".format(porc_A, porc_C, porc_G, porc_T))
+else:
+    print("""A porcentagem de nucleotídeos A é: {}%
+        de nucleotídeos C é: {}%
+        de nucleotídeos G é: {}%
+        de nucleotídeos T é: {}%
+        A quantidade de nucleotídeos invalidos é de {} e a porcentagem de {}%.""".format(porc_A, porc_C, porc_G, porc_T, quant_inv, porc_inv))
 
     
 if __name__ == '__main__':
